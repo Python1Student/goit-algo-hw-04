7cc388268b46422991fad90585bf65f2646d78ba
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-18 20:06:17 +0000

    First_save

---
+++ Task_1/main.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import sys
+
+def total_selery(path: str) -> tuple:
+    file_path = Path(path)
+    if file_path.exists():
+        with open(file_path, 'r') as file:
+            salary = [int(num.split(',')[1].strip()) for num in file.readlines()]
+    else:
+        print('File Not Found')
+        sys.exit()
+
+    total = sum(salary)
+    average = total / len(salary)
+
+    return total, average
+
+total, average = total_selery('salary_file.txt')
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
