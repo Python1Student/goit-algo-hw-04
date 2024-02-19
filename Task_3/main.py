4d2d080629e604114f766d64a51382fe2e2cf089
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-19 23:51:15 +0000

    Final

--- Task_3/main.py
+++ Task_3/main.py
@@ -1,20 +1,25 @@
 from pathlib import Path
 from colorama import Fore, Style
+from sys import argv
 
-COLORS = (Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.LIGHTBLACK_EX)
+COLORS = (Fore.GREEN, Fore.BLUE, Fore.LIGHTBLACK_EX, Fore.RED)
 
 
-def directory(path: str, depth: int = 0) -> None:
+def directory(path: str = '', depth: int = 1, is_root: bool = False) -> None:
     directory_path = Path(path)
+    if is_root:
+        print('📦 ' + COLORS[-1] + directory_path.name + Style.RESET_ALL)
 
     for item in directory_path.iterdir():
         name = item.name
-        print(COLORS[6] + '-' * depth + Style.RESET_ALL, end='')
+        print(COLORS[2] + '-' * depth + Style.RESET_ALL, end='')
         if item.is_dir():
-            print(COLORS[1] + name)
+            print('📂 ' + COLORS[0] + name + Style.RESET_ALL)
             directory(item, depth + 1)
         else: 
-            print(COLORS[3] + name)
+            print('📜 ' + COLORS[1] + name + Style.RESET_ALL)
         
-
-directory('')
+if len(argv) > 1:
+    directory(argv[1], is_root=True)
+else: 
+    directory(is_root=True)
