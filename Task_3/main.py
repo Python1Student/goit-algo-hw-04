c4962e4cb990918b78540b6e2d056fe72e23b7aa
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-20 00:01:24 +0000

    Error_B

--- Task_3/main.py
+++ Task_3/main.py
@@ -7,7 +7,7 @@ def directory(path: str = '', depth: int = 1, is_root: bool = False) -> None:
     # Створюємо об'єкт Path
     directory_path = Path(path)
     if not directory_path.exists():
-        exit('Directory Not Found')
+        exit(Fore.RED + '[ERROR] ' + Style.RESET_ALL + 'Directory Not Found')
     # Перевіряємо чи функція викликається вперше
     if is_root:
         print('📦 ' + Fore.RED + directory_path.name + Style.RESET_ALL)
