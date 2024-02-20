2a5ab8c35d162199f2f9944b9565bf7cac1e84e3
Yehor Kalchenko <egorkalcenko69@gmail.com>
2024-02-20 14:20:14 +0000

    Comments

--- Task_4/main.py
+++ Task_4/main.py
@@ -9,13 +9,16 @@ def main():
         user_input = input(Fore.GREEN + '>>> ' + Style.RESET_ALL)
         command, *args = parse_input(user_input) if user_input else (None,)
 
+        # Для закриття
         if command in ('exit', 'close'):
             print(BOT + 'Good Bye!')
             break
 
+        # Вітаємося
         elif command == 'hello':
             print(BOT + 'How can I help you?')
 
+        # Додаємо або змінюємо контакт
         elif command in ('add', 'change'):
             # Перевіряємо чи 2 аргумента
             if len(args) == 2:
@@ -23,6 +26,7 @@ def main():
             else:
                 print(ERROR + 'Wrong Contact Details!')
 
+        # Для виводу одного контаку
         elif command == 'phone':
             # Перевіряємо чи є аргументи
             if args:
@@ -30,10 +34,13 @@ def main():
             else:
                 print(ERROR + 'Missed Contact Details!')
 
+        # Для виводу всіх контактів
         elif command == 'all':
             print(read_contact())
 
+        # Якщо користувач не ввів жодну з можливих команд ми даємо знати йому про це
         else: print(ERROR + 'Invalid Command')
 
+# Перевіряємо чи це точка входу
 if __name__ == '__main__':
     main()
